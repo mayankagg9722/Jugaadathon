144a60b0057c0407b3c2014d4182088fa1b6f4c4
uddish <uddishverma22@gmail.com>
2017-03-11 15:51:05 +0530

    Qr scanner working

---
+++ app/src/main/java/com/example/uddishverma22/jugaadathonhack/AdharDetailsPOJO.java
@@ -0,0 +1,11 @@
+package com.example.uddishverma22.jugaadathonhack;
+
+/**
+ * Created by uddishverma22 on 11/03/17.
+ */
+
+public class AdharDetailsPOJO {
+
+    
+
+}
--- app/src/main/java/com/example/uddishverma22/jugaadathonhack/MainActivity.java
+++ app/src/main/java/com/example/uddishverma22/jugaadathonhack/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.uddishverma22.jugaadathonhack;
 
+import android.content.Intent;
+import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
+
+public class MainActivity extends AppCompatActivity implements QRCodeReaderView.OnQRCodeReadListener{
+
+    private TextView resultTextView;
+    private QRCodeReaderView mydecoderview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mydecoderview = (QRCodeReaderView) findViewById(R.id.qrdecoderview);
+        resultTextView = (TextView) findViewById(R.id.tv1);
+        mydecoderview.setOnQRCodeReadListener(this);
+
+        // Use this function to enable/disable decoding
+        mydecoderview.setQRDecodingEnabled(true);
+
+        // Use this function to change the autofocus interval (default is 5 secs)
+        mydecoderview.setAutofocusInterval(2000L);
+
+        // Use this function to enable/disable Torch
+        mydecoderview.setTorchEnabled(true);
+
+        // Use this function to set front camera preview
+        mydecoderview.setFrontCamera();
+
+        // Use this function to set back camera preview
+        mydecoderview.setBackCamera();
+    }
+
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed in View
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        resultTextView.setText(text);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mydecoderview.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mydecoderview.stopCamera();
     }
 }
